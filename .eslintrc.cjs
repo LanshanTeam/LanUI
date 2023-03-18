ba44dd177f86e6297a1a37452f32c6e6106b85f7
wjywy <2934610933@qq.com>
2023-03-18 10:24:08 +0800

    feat: 初始化组件库

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,15 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:storybook/recommended'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['tsconfig.json']
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {}
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 
 import Button from '../components/button/index';
 

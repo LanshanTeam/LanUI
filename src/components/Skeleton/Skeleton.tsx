2eaa411634fcbf34f3700e01a8fcc6a199cae52c
Shamaralyy <2670602083@qq.com>
2023-11-20 20:20:37 +0800

    feat: 增加Skeleton组件circle属性功能

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -2,17 +2,40 @@ import { ReactNode, useEffect } from 'react';
 import './style/skeleton.less';
 
 export interface SkeletonProps {
-    children: ReactNode;
+    loading: Boolean;
+    height?: string;
+    size?: number;
+    circle?: Boolean;
+    children?: ReactNode;
 }
 
 const Skeleton = (props: SkeletonProps) => {
-    const { children } = props;
+    const { loading, height = '30px', circle, size = 6, children } = props;
 
     useEffect(() => {
-        console.log(children);
-    });
+        validateSkeletonProps(props);
+    }, []);
 
-    return <div className="loading"></div>;
+    const validateSkeletonProps = (props: SkeletonProps): void => {
+        if (props.size !== undefined && props.circle === undefined) {
+            throw new Error(
+                'If size is provided, circle must also be provided.'
+            );
+        }
+    };
+
+    return loading ? (
+        <div
+            className="loading"
+            style={{
+                width: circle ? 5 * size + 'px' : '100%',
+                height: circle ? 5 * size + 'px' : height,
+                borderRadius: circle ? '50%' : 0,
+            }}
+        ></div>
+    ) : (
+        <>{children}</>
+    );
 };
 
 export default Skeleton;
--- src/stories/Skeleton.stories.tsx
+++ src/stories/Skeleton.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { type ComponentMeta } from '@storybook/react';
 
 import Skeleton from '../components/Skeleton/index';
+import { Button } from 'lanblue';
 
 export default {
     title: 'Example/Skeleton',
@@ -9,14 +10,42 @@ export default {
     argTypes: {},
 } as ComponentMeta<typeof Skeleton>;
 
-export const primary = () => {
+export const Primary = () => {
     return (
-        <Skeleton>
-            <h4>CodingStartup</h4>
-            <div className="description">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
-                quasienim facere commodi omnis. . .
-            </div>
-        </Skeleton>
+        <>
+            <Skeleton loading={true} height="20px"></Skeleton>
+            <Skeleton loading={true} height="20px"></Skeleton>
+            <Skeleton loading={true} height="20px"></Skeleton>
+        </>
+    );
+};
+
+export const Circle = () => {
+    return (
+        <>
+            <Skeleton loading={true} circle={true} size={10}></Skeleton>
+        </>
+    );
+};
+
+export const basic = () => {
+    const [loading, setLoading] = useState(false);
+    const toggle = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    };
+    return (
+        <>
+            <Button label="toggle" onClick={toggle}></Button>
+            <Skeleton loading={loading}>
+                <h4>CodingStartup</h4>
+                <div className="description">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
+                    quasienim facere commodi omnis. . .
+                </div>
+            </Skeleton>
+        </>
     );
 };
